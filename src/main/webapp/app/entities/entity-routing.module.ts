5961fb9c8bb9d32a38f1097cd2c4f6238b6e7660
ludwig saux <ludwig.saux@hotmail.com>
2022-03-08 10:21:51 +0100

    Deploy to Heroku

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,11 +4,6 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
-      {
-        path: 'contact',
-        data: { pageTitle: 'myApp.contact.home.title' },
-        loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule),
-      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
